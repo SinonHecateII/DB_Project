5c4c5c2a582e8c6da03c8be91771824ce93ea6b7
SinonHecateII <61343001+SinonHecateII@users.noreply.github.com>
2022-11-08 22:59:45 +0900

    Data Input to DB

--- BackEnd/DB/Data_Input.py
+++ BackEnd/DB/Data_Input.py
@@ -3,6 +3,7 @@ import requests, json
 
 def readDatabase(databaseId, headers):
     readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
+    url = ImportantData.PostURL
 
     res = requests.post(readUrl, headers = headers)
     print(res.status_code)
@@ -13,9 +14,18 @@ def readDatabase(databaseId, headers):
         try:
             properties = result['properties']
             Restaurant_Name = properties['가게명']['title'][0]['text']['content']
-            Restaurant_Vibe = properties['분위기']['multi_select'][0]['name']
+            Restaurant_Mood = properties['분위기']['multi_select'][0]['name']
             Restaurant_Loc = properties['위치']['multi_select'][0]['name']
-            print(f'가게이름: {Restaurant_Name}, 분위기: {Restaurant_Vibe}, 위치: {Restaurant_Loc}')
+            print(f'가게이름: {Restaurant_Name}, 분위기: {Restaurant_Mood}, 위치: {Restaurant_Loc}')
+
+            datas = {
+                "location" : Restaurant_Loc,
+                "name" : Restaurant_Name,
+                "mood" : Restaurant_Mood,
+                "photoCnt" : 0
+            }
+            response = requests.post(url, data = json.dumps(datas))
+
         except:
             continue
 
