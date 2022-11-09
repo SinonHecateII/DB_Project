a814686c44d40bd436744a604ac4c54f7579ec68
915_랩실컴 <yoon6763@naver.com>
2022-11-09 13:37:09 +0900

    어댑터 적용 및 샘플코드 추가

--- FrontEnd/app/src/main/java/com/example/deliciousfood/pages/MainActivity.java
+++ FrontEnd/app/src/main/java/com/example/deliciousfood/pages/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +14,10 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.deliciousfood.R;
+import com.example.deliciousfood.adapter.RestaurantViewPagerAdapter;
 import com.example.deliciousfood.api.DeliciousAPI;
 import com.example.deliciousfood.databinding.ActivityMainBinding;
+import com.example.deliciousfood.utils.Constants;
 import com.example.deliciousfood.utils.SharedPreferenceHelper;
 import com.google.android.material.tabs.TabLayout;
 
@@ -23,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -40,6 +44,33 @@ public class MainActivity extends AppCompatActivity {
 
         String imageStr = "http://210.125.212.207/shareimage/pasta.jpg";
         //Glide.with(this).load(imageStr).into(binding.ivImageTest);
+
+
+        // 위치
+        ArrayList<String> restaurantName = new ArrayList<>();
+        restaurantName.add("두정 먹자골목");
+        restaurantName.add("두정역 인근");
+        restaurantName.add("신부동");
+        restaurantName.add("불당 카페거리");
+
+        // ViewPager 에 쓸 프래그먼트
+        ArrayList<Fragment> fragments = new ArrayList<>();
+
+        for (int i = 0; i < restaurantName.size(); i++) {
+            fragments.add(new RestaurantFragment());
+
+            // 액티비티 -> 프래그먼트 값 전달
+            // 번들에 String 값을 입력하고 Argument 로 번들을 전달함
+            Bundle bundle = new Bundle();
+            bundle.putString(Constants.ARGUMENT_LOCATION, restaurantName.get(i));
+            fragments.get(i).setArguments(bundle);
+        }
+
+        // 뷰페이저 어댑터
+        RestaurantViewPagerAdapter viewPagerAdapter = new RestaurantViewPagerAdapter(fragments, restaurantName, getSupportFragmentManager());
+
+        binding.vpRestaurant.setAdapter(viewPagerAdapter); // Layout(xml) 의 ViewPager 객체에 어댑터를 set
+        binding.tlRestaurant.setupWithViewPager(binding.vpRestaurant); // Layout(xml) 의 TabLayout 을 ViewPager 객체와 연동함
 
     }
 }
