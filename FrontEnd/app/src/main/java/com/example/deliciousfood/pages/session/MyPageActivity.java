9bd26c9bd2b52735bacbe938e82489009d890527
SinonHecateII <61343001+SinonHecateII@users.noreply.github.com>
2022-11-30 15:15:14 +0900

    MyPage 유저 닉네임 가져오기 추가

--- FrontEnd/app/src/main/java/com/example/deliciousfood/pages/session/MyPageActivity.java
+++ FrontEnd/app/src/main/java/com/example/deliciousfood/pages/session/MyPageActivity.java
@@ -3,15 +3,22 @@ package com.example.deliciousfood.pages.session;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.deliciousfood.R;
+import com.example.deliciousfood.api.dto.responseDTO.LoginResponseDTO;
 import com.example.deliciousfood.databinding.ActivityMyPageBinding;
+import com.example.deliciousfood.utils.SharedPreferenceHelper;
 
 public class MyPageActivity extends AppCompatActivity {
     private ActivityMyPageBinding binding;
-
+    TextView UserName;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,13 @@ public class MyPageActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbarMypage);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        editor = pref.edit();
 
+        // 유저 Nickname 가져오기
+        String name = SharedPreferenceHelper.INSTANCE.getNickname(getApplicationContext());
+        UserName = (TextView) findViewById(R.id.tv_mypage_nickname);
+        UserName.setText(name);
 
     }
 
--- FrontEnd/app/src/main/java/com/example/deliciousfood/utils/SharedPreferenceHelper.kt
+++ FrontEnd/app/src/main/java/com/example/deliciousfood/utils/SharedPreferenceHelper.kt
@@ -6,6 +6,7 @@ import android.content.Context.MODE_PRIVATE
 object SharedPreferenceHelper {
     private const val PREF_APP = "DELICIOUS_APP"
     private const val SP_ID = "SP_ID"
+    private const val SP_Nickname = "SP_Nickname";
 
     // 로그인한 아이디
     fun setLoginID(context: Context, id: String) {
@@ -15,9 +16,23 @@ object SharedPreferenceHelper {
         editor.commit()
     }
 
+    // 유저 닉네임
+    fun setNickname(context: Context, name: String) {
+        val pref = context.getSharedPreferences(PREF_APP, MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putString(SP_Nickname, name)
+        editor.commit()
+    }
+
     // 로그인한 아이디 받기
     fun getLoginID(context: Context): String? {
         val pref = context.getSharedPreferences(PREF_APP, MODE_PRIVATE)
         return pref.getString(SP_ID, "")
+    }
+
+    // 우저 닉네임 받기
+    fun getNickname(context: Context): String? {
+        val pref = context.getSharedPreferences(PREF_APP, MODE_PRIVATE)
+        return pref.getString(SP_Nickname, "")
     }
 }
